99023a6ddffb7e54d04c5ae057dcb1e97f6f5678
David Tellez <davidctellezg@gmail.com>
2022-09-23 18:06:58 -0500

    Create brutalist library maker

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+let myLibrary = [new Book('book1', 'J.R.R.Tolkien', 100, false), new Book('book2', 'J.R.R.Tolkien', 100, false), new Book('book3', 'J.R.R.Tolkien', 100, false)];
+
 function Book(title, author, numberPages, read){
   this.title = title;
   this.author = author;
@@ -5,7 +7,26 @@ function Book(title, author, numberPages, read){
   this.read = read;
 }
 
-Book.prototype.info = function(){
-  let returnS = `${this.title} by ${this.author}, ${this.pages} pages, `;
-  return this.read === true? returnS + 'read.': returnS + 'not read yet.';
+// Book.prototype.info = function(){
+//   let returnS = `${this.title} by ${this.author}, ${this.pages} pages, `;
+//   return this.read === true? returnS + 'read.': returnS + 'not read yet.';
+// }
+
+function addBookToLibrary(book){
+  myLibrary.push(book);
 }
+
+let library = document.getElementById('library');
+
+window.addEventListener('load', () =>{
+  myLibrary.forEach((element) =>{
+    let newBook = document.createElement('div');
+    newBook.classList.add('book');
+    for (key in element){
+      let prop = document.createElement('p');
+      prop.textContent = `${key}: ${element[key]}`;
+      newBook.appendChild(prop);
+    }
+    library.appendChild(newBook);
+  })
+});
