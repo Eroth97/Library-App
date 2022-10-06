2b4525928bd1c838f1f81e7f67275589a1cfd7a1
David Tellez <davidctellezg@gmail.com>
2022-10-06 17:48:02 -0500

    Add dynamic erase and read buttons

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let myLibrary = [new Book('book1', 'J.R.R.Tolkien', 100, false), new Book('book2', 'J.R.R.Tolkien', 100, false), new Book('book3', 'J.R.R.Tolkien', 100, false)];
+let myLibrary = [new Book('book1', 'J.R.R.Tolkien', 100, true), new Book('book2', 'J.R.R.Tolkien', 100, false), new Book('book3', 'J.R.R.Tolkien', 100, false)];
 
 function Book(title, author, numberPages, read){
   this.title = title;
@@ -29,82 +29,12 @@ let bookMaker = document.getElementById('book-maker');
 let formBookMaker = document.getElementById('form-book-maker');
 let submitButton = document.getElementById('submit-button');
 
-//Functions
+//Non native Functions
 function capitalizeFirstWord(word){
   return word[0].toUpperCase() + word.slice(1);
 }
 
-function addBookToLibrary(book){
-  myLibrary.push(book);
-};
 
-function displayBooks(){
-  myLibrary.forEach((element) =>{
-    numberOfBooks++;
-    let bookCard = document.createElement('div');
-
-    bookCard.setAttribute('id', `book ${numberOfBooks}`);
-
-    let eraseButton = createEraseButton(numberOfBooks);
-    let readButton = createReadButton();
-
-    bookCard.classList.add('book');
-    for (key in element){
-      let prop = document.createElement('p');
-      if (key === 'read'){
-        let bookReadStatus = element[key] === false? 'No': 'Yes';
-        prop.textContent = `${capitalizeFirstWord(key)}:  ${bookReadStatus}`;
-      }
-      else prop.textContent = `${capitalizeFirstWord(key)}:  ${element[key]}`;
-      
-      bookCard.appendChild(prop);
-      bookCard.appendChild(eraseButton);
-      bookCard.appendChild(readButton);
-    }
-    library.appendChild(bookCard);
-  })
-};
-
-function createEraseButton(num){
-  let button = document.createElement('button');
-  button.textContent = 'Erase';
-  button.setAttribute('id', `erase-book-${num}`);
-  button.addEventListener('click', () => {
-    //This will be one function
-    let bookCard = document.getElementById(`book ${num}`);
-    library.removeChild(bookCard);
-    num === myLibrary.length? myLibrary = myLibrary.slice(0, num-1): 
-                              myLibrary = myLibrary.slice(0, num).concat(myLibrary.slice(num + 1));
-    console.log(myLibrary);
-
-    //This will be other function.
-    numberOfBooks = 0;
-    let books = document.querySelectorAll('.book');
-    books.forEach((book) => {
-      numberOfBooks++;
-      book.setAttribute('id', `book ${numberOfBooks}`);
-    })
-
-  });
-  return button;
-}
-
-function createReadButton(){
-  let button = document.createElement('button');
-  button.textContent = 'Now Read';
-  return button;
-}
-
-function eraseBooks(){
-  numberOfBooks = 0;
-  let bookCards = document.querySelectorAll('.book');
-  bookCards.forEach(element => library.removeChild(element));
-}
-
-function cleanForm(){
-  textInputs.forEach(element => element.value = '');
-  buttonInputs.forEach(element => element.checked = false);
-}
 
 //This chunk of code prints all the books on screen.
 window.addEventListener('load', () => displayBooks());
@@ -129,7 +59,7 @@ submitButton.addEventListener('click', () => bookMaker.style.display = '');
 
 
 
-//This chunk of code creates a book, using the form provided in the form.
+//This chunk of code creates a book, using the information provided by the user.
 submitButton.addEventListener('click', () =>{
   let readStatus = yes.checked === true? true: false;
 
@@ -140,3 +70,89 @@ submitButton.addEventListener('click', () =>{
   displayBooks();
   cleanForm();
 })
+
+//Functionality behind the pseudo-submission system.
+
+function addBookToLibrary(book){
+  myLibrary.push(book);
+};
+
+function eraseBooks(){
+  numberOfBooks = 0;
+  let bookCards = document.querySelectorAll('.book');
+  bookCards.forEach(element => library.removeChild(element));
+}
+
+function displayBooks(){
+  myLibrary.forEach((element) =>{
+    numberOfBooks++;
+    let bookCard = document.createElement('div');
+
+    bookCard.setAttribute('id', `book ${numberOfBooks}`);
+
+    let eraseButton = createEraseButton(numberOfBooks);
+    let readButton = createReadButton(numberOfBooks);
+
+    bookCard.classList.add('book');
+    for (key in element){
+      let prop = document.createElement('p');
+      if (key === 'read'){
+        let bookReadStatus = element[key] === false? 'No': 'Yes';
+        prop.textContent = `${capitalizeFirstWord(key)}:  ${bookReadStatus}`;
+      }
+      else prop.textContent = `${capitalizeFirstWord(key)}:  ${element[key]}`;
+      
+      bookCard.appendChild(prop);
+      bookCard.appendChild(eraseButton);
+      bookCard.appendChild(readButton);
+    }
+    library.appendChild(bookCard);
+  })
+};
+
+
+function cleanForm(){
+  textInputs.forEach(element => element.value = '');
+  buttonInputs.forEach(element => element.checked = false);
+}
+
+//Functionality behind display Books function.
+
+//Functionality Erase Buttons
+function createEraseButton(num){
+  let button = document.createElement('button');
+  button.textContent = 'Erase';
+  button.setAttribute('id', `erase-book-${num}`);
+  button.addEventListener('click', () => {
+    updateLibrary(num);
+    updateBookCardsIDs();        
+  });
+  return button;
+}
+
+function updateLibrary(num){
+  let bookCard = document.getElementById(`book ${num}`);
+  library.removeChild(bookCard);
+  num === myLibrary.length? myLibrary = myLibrary.slice(0, num-1): 
+                              myLibrary = myLibrary.slice(0, num).concat(myLibrary.slice(num + 1));
+}
+
+function updateBookCardsIDs(){
+  numberOfBooks = 0;
+  let books = document.querySelectorAll('.book');
+  books.forEach((book) => {
+    numberOfBooks++;
+    book.setAttribute('id', `book ${numberOfBooks}`);
+
+    let button = book.children[4];
+    button.setAttribute('id', `erase-book-${numberOfBooks}`);
+    })
+}
+
+//Functionality Read Buttons
+function createReadButton(num){
+  let button = document.createElement('button');
+  button.textContent = 'Now Read';
+  myLibrary[num - 1].read === false? button.disabled = false: button.disabled = true; 
+  return button;
+}
